b2397480846e54e02a912130abf049916c6bf266
Sumi Seo <qkobr94@gmail.com>
2022-04-24 21:03:59 +0900

    feat(pagination): Add pagination

--- src/components/Planets.tsx
+++ src/components/Planets.tsx
@@ -1,21 +1,24 @@
+import { useState } from "react";
 import { useQuery } from "react-query";
 import Planet from "./Planet";
-
-const fetchPlanets = async () => {
-  const res = await fetch("http://swapi.dev/api/planets/");
-  return res.json();
-};
-
 interface Planet {
   name: string;
   population: string;
   terrain: string;
 }
+
 const Planets = (): JSX.Element => {
-  const { data, status } = useQuery("planets", fetchPlanets, {
-    staleTime: 0,
-    onSuccess: () => console.log("data is fetched"),
-  });
+  const [page, setPage] = useState(1);
+  const fetchPlanets = async (pageNum: number = 0) => {
+    const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
+    return res.json();
+  };
+  const { status, data, error, isFetching, isPreviousData } = useQuery(
+    ["projects", page],
+    () => fetchPlanets(page),
+    { keepPreviousData: true, staleTime: 5000 }
+  );
+
   console.log(data);
 
   return (
